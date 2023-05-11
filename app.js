70019c6179bb1c0dcc3a9e20e454c1ce4ad12fde
Anthony Asiimwe <tonyasiimwe14@gmail.com>
2023-05-11 23:51:36 +0300

    Use of v-for

--- app.js
+++ app.js
@@ -2,27 +2,16 @@ const app = Vue.createApp({
   data() {
     return {
       showBooks: true,
-      title: "The final empire",
-      author: "Brandon S",
-      age: 45,
-      x: 0,
-      y: 0,
+      books: [
+        { title: "name of the wind", author: "patrick rothfuss" },
+        { title: "way of the wind", author: "roy rothfuss" },
+        { title: "story of the wind", author: "brandon rothfuss" },
+      ],
     };
   },
   methods: {
-    changeTitle(title) {
-      //   this.title = "words of radiance";
-      this.title = title;
-    },
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
-    },
-    handleEvent(e, data) {
-      console.log(e, e.type);
-    },
-    handleMousemove(e) {
-      this.x = e.offsetX;
-      this.y = e.offsetY;
     },
   },
 });
