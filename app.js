940c98d1d9215caa328460f71a7655705bd19241
Anthony Asiimwe <tonyasiimwe14@gmail.com>
2023-05-13 10:57:28 +0300

    Use of different js methods

--- app.js
+++ app.js
@@ -1,17 +1,41 @@
 const app = Vue.createApp({
   data() {
     return {
+      url: "http://www.thenetninja.co.uk",
       showBooks: true,
       books: [
-        { title: "name of the wind", author: "patrick rothfuss" },
-        { title: "way of the wind", author: "roy rothfuss" },
-        { title: "story of the wind", author: "brandon rothfuss" },
+        {
+          title: "name of the wind",
+          author: "patrick rothfuss",
+          img: "assets/1.jpg",
+          isFav: true,
+        },
+        {
+          title: "way of the wind",
+          author: "roy rothfuss",
+          img: "assets/2.jpg",
+          isFav: false,
+        },
+        {
+          title: "story of the wind",
+          author: "brandon rothfuss",
+          img: "assets/3.jpg",
+          isFav: true,
+        },
       ],
     };
   },
   methods: {
     toggleShowBooks() {
       this.showBooks = !this.showBooks;
+    },
+    toggleFav(book) {
+      book.isFav = !book.isFav;
+    },
+  },
+  computed: {
+    filteredBooks() {
+      return this.books.filter((book) => book.isFav);
     },
   },
 });
